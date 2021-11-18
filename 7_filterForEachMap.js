d1b948af8fd4e12cba3c477a2ce6383ba3432226
AltaraceO <orialtarace@gmail.com>
2021-11-18 21:21:00 +0200

    forEach Map and Filter reveiw

---
+++ 7_filterForEachMap.js
@@ -0,0 +1,39 @@
+let myRandomArray = [
+  "The",
+  "quick",
+  "brown",
+  "fox",
+  "jumps",
+  "over",
+  "the",
+  "lazy",
+  "dog",
+];
+let newArr = [];
+
+// forEach --->
+
+myRandomArray.forEach(function (item, idx) {
+  if (idx % 2 === 0) {
+    newArr.push(item.toUpperCase());
+  }
+  return;
+});
+console.log(newArr);
+console.log(myRandomArray); //the original arr remains unchanged
+
+// Map --->
+
+const mappedRandom = myRandomArray.map(function (item) {
+  return item.split("").join("-");
+});
+
+console.log(mappedRandom);
+
+// Filter --->
+numbers = [1, 2, 3, 4];
+const letterE = myRandomArray.filter(
+  (word) => word === "the" || word === "The"
+);
+
+console.log(letterE);
